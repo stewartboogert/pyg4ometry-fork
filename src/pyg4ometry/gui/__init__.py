69346aaceb97b3bc9ba0eac5feaf58bc07f2e26b
Stewart Boogert <stewart.boogert@cockcroft.ac.uk>
2024-09-01 20:44:58 +0100

    gui : restart Qt gui for geometery authoring

--- src/pyg4ometry/gui/__init__.py
+++ src/pyg4ometry/gui/__init__.py
@@ -1,3 +1,7 @@
-from .MainWindow import *
-from .QVTKRenderWindowInteractor import *
-from .GeometryModel import *
+from .EditorWindow import *
+from .ModelGeant4 import *
+from .VtkWidget import *
+
+# from .MainWindow import *
+# from .QVTKRenderWindowInteractor import *
+# from .GeometryModel import *
