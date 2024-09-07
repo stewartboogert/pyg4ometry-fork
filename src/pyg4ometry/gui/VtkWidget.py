2b414e7363dfe5c01558fd1ff6fb24450739ac39
Stewart Boogert <stewart.boogert@cockcroft.ac.uk>
2024-09-07 22:01:33 +0100

    gui : vtk in Qt window

--- src/pyg4ometry/gui/VtkWidget.py
+++ src/pyg4ometry/gui/VtkWidget.py
@@ -1,35 +1,16 @@
 import vtk
 
+import pyg4ometry.visualisation.VtkViewerNew as _VtkViewerNew
+
 from vtk.qt.QVTKRenderWindowInteractor import (
     QVTKRenderWindowInteractor as _QVTKRenderWindowInteractor,
 )
 
 
-class VtkWidget(_QVTKRenderWindowInteractor):
+class VtkWidget(_QVTKRenderWindowInteractor, _VtkViewerNew):
 
     def __init__(self):
         super().__init__()
 
-        self.ren = vtk.vtkRenderer()
         self.GetRenderWindow().AddRenderer(self.ren)
         self.iren = self.GetRenderWindow().GetInteractor()
-
-        # Create source
-        source = vtk.vtkSphereSource()
-        source.SetCenter(0, 0, 0)
-        source.SetRadius(5.0)
-
-        # Create a mapper
-        mapper = vtk.vtkPolyDataMapper()
-        mapper.SetInputConnection(source.GetOutputPort())
-
-        # Create an actor
-        actor = vtk.vtkActor()
-        actor.SetMapper(mapper)
-
-        self.ren.AddActor(actor)
-
-        self.ren.ResetCamera()
-
-        self.show()
-        self.iren.Initialize()
