656ec1e55fc5e548f5a60063a9889d08e6e4246c
Stewart Boogert <stewart.boogert@cockcroft.ac.uk>
2024-09-08 12:17:33 +0100

    gui : guard against missing import

--- src/pyg4ometry/gui/__init__.py
+++ src/pyg4ometry/gui/__init__.py
@@ -1,6 +1,9 @@
-from .EditorWindow import *
-from .ModelGeant4 import *
-from .VtkWidget import *
+try:
+    from .EditorWindow import *
+    from .ModelGeant4 import *
+    from .VtkWidget import *
+except ImportError:
+    print("Need PyQt5")
 
 # from .MainWindow import *
 # from .QVTKRenderWindowInteractor import *
