94e198a0f5d81fbdcb670dca5ce3017dc4af148d
Stewart Boogert <stewart.boogert@cockcroft.ac.uk>
2024-09-26 18:49:01 +0100

    vis: Don't invent a name for the world mesh

--- src/pyg4ometry/visualisation/ViewerBase.py
+++ src/pyg4ometry/visualisation/ViewerBase.py
@@ -148,7 +148,7 @@ class ViewerBase:
 
             # add instance
             if name is None:
-                name = "world"
+                name = lv.name
             self.addInstance(lv.name, mtra, tra, name)
 
             vo = self.getVisOptionsLV(lv)
