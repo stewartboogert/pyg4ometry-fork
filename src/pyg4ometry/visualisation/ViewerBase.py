98ab3552bb16af1ad7062f668973d458c1d85d14
Stewart Boogert <stewart.boogert@cockcroft.ac.uk>
2024-09-26 19:26:50 +0100

    vis: remove spurious printout and intronic code

--- src/pyg4ometry/visualisation/ViewerBase.py
+++ src/pyg4ometry/visualisation/ViewerBase.py
@@ -430,11 +430,6 @@ class ViewerBase:
                 elif includeWireframe:
                     aabb.union(obb.getAABB())
 
-        print(aabb)
-        # print(v)
-        # if v.representation != "wireframe" :
-        #    print(aabb)
-
         return aabb
 
     def exportGLTFScene(self, gltfFileName="test.gltf", singleInstance=False):
