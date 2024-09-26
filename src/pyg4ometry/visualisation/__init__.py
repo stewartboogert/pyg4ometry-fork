0cfab782308dfa7827fce0a76686ea9b00bfe669
Stewart Boogert <stewart.boogert@cockcroft.ac.uk>
2024-09-26 22:26:25 +0100

    vis: no longer needed activated in VtkViewerNew

--- src/pyg4ometry/visualisation/__init__.py
+++ src/pyg4ometry/visualisation/__init__.py
@@ -6,7 +6,6 @@ from .ViewerBase import ViewerBase
 from .VisualisationOptions import *
 from .VtkViewer import *
 from .VtkViewerNew import *
-from .VtkViewerPBR import *
 from .BlenderViewer import *
 from .RenderWriter import *
 from .Convert import *
